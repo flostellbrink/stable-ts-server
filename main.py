5e54411829bf4eeae9d71c7e80f2ad2f6478cb9c
Florian Stellbrink <flo@stellbr.ink>
2024-07-21 19:13:01 +0200

    Clean up response typing

--- main.py
+++ main.py
@@ -1,50 +1,67 @@
 import os
 import stable_whisper
 from typing import Annotated
+from pydantic import BaseModel
 from fastapi import FastAPI, UploadFile, Form
 from fastapi.openapi.utils import get_openapi
 import json
 
-app = FastAPI()
+class WhisperWord(BaseModel):
+    word: str
+    start: float
+    end: float
+    probability: float
 
-class Segment:
-    def __init__(self, start, end, text):
-        self.start = start
-        self.end = end
-        self.text = text
+class WhisperSegment(BaseModel):
+    id: int
+    seek: int
+    start: float
+    end: float
+    text: str
+    tokens: list
+    temperature: float
+    avgLogprob: float
+    compressionRatio: float
+    noSpeechProb: float
+    words: list[WhisperWord]
 
-def toStandardWhisperResult(result: stable_whisper.result.WhisperResult):
-    return {
-        "text": result.text,
-        "segments": [
-            {
-                "id": i,
-                "seek": 0,
-                "start": segment.start,
-                "end": segment.end,
-                "text": segment.text,
-                "tokens": [],
-                "temperature": 0,
-                "avgLogprob": 0,
-                "compressionRatio": 0,
-                "noSpeechProb": 0,
-                "words": [
-                    {
-                        "word": word.word,
-                        "start": word.start,
-                        "end": word.end,
-                        "probability": word.probability
-                    }
+class WhisperResult(BaseModel):
+    text: str
+    segments: list[WhisperSegment]
+
+def toStandardWhisperResult(result: stable_whisper.result.WhisperResult) -> WhisperResult:
+    return WhisperResult(
+        text=result.text,
+        segments=[
+            WhisperSegment(
+                id=i,
+                seek=0,
+                start=segment.start,
+                end=segment.end,
+                text=segment.text,
+                tokens=[],
+                temperature=0,
+                avgLogprob=0,
+                compressionRatio=0,
+                noSpeechProb=0,
+                words=[
+                    WhisperWord(
+                        word=word.word,
+                        start=word.start,
+                        end=word.end,
+                        probability=word.probability
+                    )
                     for word in segment.words
                 ]
-            }
+            )
             for i, segment in enumerate(result.segments)
-        ],
-        "language": result.language
-    }
+        ]
+    )
 
-@app.post("/api/align")
-async def align_text_with_audio(audio: UploadFile, text: UploadFile, language: Annotated[str, Form(examples=["en"])], offset: Annotated[int, Form()] = 0):
+app = FastAPI()
+
+@app.post("/api/align", response_model=WhisperResult)
+async def align_text_with_audio(audio: UploadFile, text: UploadFile, language: Annotated[str, Form(examples=["en"])]):
     audioRaw = await audio.read()
     textRaw = await text.read()
     textUtf8 = textRaw.decode("utf-8")
